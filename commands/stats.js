de56bed6e068da47e1c3f65d6e10532d2875a0a4
Emissions <unitedrealm@hotmail.com>
2019-11-13 20:17:14 +0000

    Updated stats.js (command)

--- commands/stats.js
+++ commands/stats.js
@@ -52,9 +52,34 @@ module.exports = class stats {
       });
    }
 
+   guildStats(client) {
+      return new Promise((re, r) => {
+         client.models.guildStats
+            .find({})
+            .lean()
+            .exec((err, docs) => {
+               if (err) return r(err);
+               let sumsells = 0;
+               let sumlevelups = 0;
+               let sumpoints = 0;
+               for (const doc of docs) {
+                  sumsells += doc.sum_sells;
+                  sumlevelups += doc.sum_levelups;
+                  sumpoints += doc.sum_points;
+               }
+               return re({
+                  sumsells: sumsells,
+                  sumlevelups: sumlevelups,
+                  sumpoints: sumpoints
+               });
+            });
+      });
+   }
+
    async run(client, message, args) {
       const guildMines = await this.guildMines(client);
       const userProfiles = await this.userProfiles(client);
+      const guildStats = await this.guildStats(client);
       message.channel.send(
          new client.modules.Discord.MessageEmbed()
             .setColor(message.guild.me.displayHexColor)
@@ -81,6 +106,17 @@ module.exports = class stats {
             )
             .addField(`Total amount of prestiges:`, guildMines.sumprest, true)
             .addField(`Total amount of rebirths:`, userProfiles.rebirths, true)
+            .addField(`Total amount of sells:`, guildStats.sumsells, true)
+            .addField(
+               `Total amount of mine level ups:`,
+               guildStats.sumlevelups,
+               true
+            )
+            .addField(
+               `Total amount of points earned:`,
+               guildStats.sumpoints,
+               true
+            )
       );
    }
 };
