d271b12593b12f058b919e5d1ba56066537fff9e
Emissions <unitedrealm@hotmail.com>
2019-11-13 20:08:13 +0000

    Updated guildStats.js (model)

--- models/guildStats.js
+++ models/guildStats.js
@@ -1,17 +1,30 @@
 const modules = require(`../modules`);
-module.exports = modules.mongoose.model(`guildStats`, new modules.mongoose.Schema({
-    guild_id: {
-        type: String,
-        required: true
-    },
-    sum_rebirths: {
-        type: Number,
-        required: false,
-        default: 0
-    },
-    sum_sells: {
-        type: Number,
-        required: false,
-        default: 0
-    }
-}));
+module.exports = modules.mongoose.model(
+   `guildStats`,
+   new modules.mongoose.Schema({
+      guild_id: {
+         type: String,
+         required: true
+      },
+      sum_rebirths: {
+         type: Number,
+         required: false,
+         default: 0
+      },
+      sum_sells: {
+         type: Number,
+         required: false,
+         default: 0
+      },
+      sum_levelups: {
+         type: Number,
+         required: false,
+         default: 0
+      },
+      sum_points: {
+         type: Number,
+         required: false,
+         default: 0
+      }
+   })
+);
