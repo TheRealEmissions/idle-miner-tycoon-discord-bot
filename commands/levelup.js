aa80dd1eb6b16d7dd8669460693e84388409ae46
Emissions <unitedrealm@hotmail.com>
2019-11-13 20:21:14 +0000

    Added levelup.js (command)

---
+++ commands/levelup.js
@@ -0,0 +1,10 @@
+module.exports = class {
+   constructor() {
+      this.name = "levelup";
+      this.alias = ["lu"];
+      this.usage = ".levelup";
+      this.description = "Level up your currently selected mine";
+   }
+
+   async run(client, message, args) {}
+};
