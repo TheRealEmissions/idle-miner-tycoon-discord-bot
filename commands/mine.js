7ac7957c474d833d551d963394c81ab13032c440
Emissions <unitedrealm@hotmail.com>
2019-11-13 21:07:27 +0000

    Updated mine.js (command)

--- commands/mine.js
+++ commands/mine.js
@@ -165,7 +165,6 @@ module.exports = class {
                ]
             }
          };
-         message.channel.send(JSON.stringify(mine));
          message.channel.send(embed);
       } else {
          message.channel.send(
